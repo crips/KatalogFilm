ad05b6e2cbe3e9b7e1204745fa64f34f03a95d84
crips <ferryafrizal@outlook.co.id>
2016-06-12 15:35:30 +0700

    Add project files.

---
+++ KatalogFilm/Controllers/FilmsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KatalogFilm.Models;
+
+namespace KatalogFilm.Controllers
+{
+    [Authorize]
+    public class FilmsController : Controller
+    {
+        private DbFilmEntities db = new DbFilmEntities();
+
+        // GET: Films
+        public ActionResult Index()
+        {
+            var films = db.Films.Include(f => f.Genre);
+            return View(films.ToList());
+        }
+
+        // GET: Films/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film film = db.Films.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            return View(film);
+        }
+
+        // GET: Films/Create
+        public ActionResult Create()
+        {
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "NamaGenre");
+            return View();
+        }
+
+        // POST: Films/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdFilm,GenreId,NamaFilm,TahunRilis")] Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Films.Add(film);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "NamaGenre", film.GenreId);
+            return View(film);
+        }
+
+        // GET: Films/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film film = db.Films.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "NamaGenre", film.GenreId);
+            return View(film);
+        }
+
+        // POST: Films/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdFilm,GenreId,NamaFilm,TahunRilis")] Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(film).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "NamaGenre", film.GenreId);
+            return View(film);
+        }
+
+        // GET: Films/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film film = db.Films.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            return View(film);
+        }
+
+        // POST: Films/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Film film = db.Films.Find(id);
+            db.Films.Remove(film);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ KatalogFilm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KatalogFilm.Startup))]
+namespace KatalogFilm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
